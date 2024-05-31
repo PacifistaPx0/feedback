8a9adbe306d109510e20176aa6936608bd12e0d3
PacifistaPX0 <richieadigwe@gmail.com>
2024-05-31 10:38:06 +0100

    working with forms manually

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index-page" )
+    path("", views.index, name="index-page" ),
+    path("thankyou", views.thankyou, name="thankyou-page")
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 
 def index(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        print(username)
+        return HttpResponseRedirect("/thankyou")
+
     return render(request, "reviews/index.html")
+
+
+def thankyou(request):
+    return render(request, "reviews/thankyou.html")
