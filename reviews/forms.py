69d3422cd8722fa24c37f594d40d952762724b21
PacifistaPX0 <richieadigwe@gmail.com>
2024-06-03 21:38:12 +0100

    working with ModelForm

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+from .models import Review
 
-class ReviewForm(forms.Form):
+
+"""class ReviewForm(forms.Form):
     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
         "required": "Your name must not be empty",
         "max_length": "Please enter a shorter name"
@@ -16,4 +18,10 @@ class ReviewForm(forms.Form):
             MaxValueValidator(5.0)
         ],
         widget=forms.NumberInput(attrs={'step': '0.1'})
-    )
+    )"""
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"
+        # exlcude = ["owner_comment"]
