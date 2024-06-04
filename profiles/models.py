2ff94cd485ed81aba96528ee6624d14351ce07f0
PacifistaPX0 <richieadigwe@gmail.com>
2024-06-04 21:35:27 +0100

    working with forms.py

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+#considered bad practice to store files in database, so we only store the path to the file
+class UserProfile(models.Model):
+    image = models.FileField(upload_to="images")
